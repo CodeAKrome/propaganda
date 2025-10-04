cf6615403fad66109a8d9e937c14ae8b1cbd329d
kyle gaspar <kyleg.job@gmail.com>
2025-10-04 19:54:20 -0400

    syn

--- db/mkrunllms.sh
+++ db/mkrunllms.sh
@@ -1,1 +1,1 @@
-cat llms.txt| perl -e 'while (<>) {chomp; print "echo \"\\n--\\n$_\\n--\\n\"\n";print "cat tmp_q.txt tmp_vec.txt | ollama run --verbose $_\n";}' > runllms.sh
+cat llms.txt| perl -e 'while (<>) {chomp; print "echo \"\\n--\\n$_\\n--\\n\"\n";print "cat testprompt.txt | ollama run --hidethinking --verbose $_\n";}' > runllms.sh
