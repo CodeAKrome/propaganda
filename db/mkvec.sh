4107b4558884cf11f1229e60fd5c1c36cd3d4ef4
kyle gaspar <kyleg.job@gmail.com>
2025-11-19 16:56:50 -0500

    syn

---
+++ db/mkvec.sh
@@ -0,0 +1,43 @@
+startdate="$1"
+filename="$2"
+entity="$3"
+query="$4"
+fulltext=${5:-}
+
+# -----------------------------
+# 3. Check input
+# -----------------------------
+if [[ -z "$query" ]]; then
+  echo "Usage: $0 <date offset> <filebase> <entities> <query>"
+  exit 1
+fi
+
+vec="output/${filename}.vec"
+topn=40
+idfile="output/article_ids.txt"
+
+printf "\n------------- $startdate days ----------------\n"
+
+common_args=(
+    "$query"
+    --bm25
+    --orentity "$entity"
+    --start-date "$startdate"
+    --showentity
+    -n "$topn"
+    --ids "$idfile"
+)
+
+# -- Hybrid search
+if [[ -z "$fulltext" ]]; then
+    printf "VECTOR %s Entity %s only search\n" "$filename" "$entity"
+    ./hybrid.py "${common_args[@]}" > "$vec"
+else
+    text="${fulltext#+}" # remove leading '+' if present
+    filter_arg=$([[ $fulltext == +* ]] && echo "--filter" || echo "")
+
+    mode=$([[ -z "$filter_arg" ]] && echo "UNION" || echo "PREFILTER")
+    printf "%s %s Entity %s Full text %s search\n" "$mode" "$filename" "$entity" "$text"
+    
+    ./hybrid.py "${common_args[@]}" --fulltext "$text" $filter_arg > "$vec"
+fi
---
+++ llm/start_llama-server.sh
@@ -0,0 +1,1 @@
+llama-server -hf $1 -c 0 --host 127.0.0.1 --port 8033
