1c228e7b3103706cf1a799fb7ddc281d47368229
kyle gaspar <kyleg.job@gmail.com>
2026-02-08 17:33:33 -0500

    test_mongo2training.sh fix

--- t5/test_mongo2training.sh
+++ t5/test_mongo2training.sh
@@ -1,3 +1,2 @@
 export MONGODB_URI="mongodb://root:example@localhost:27017/"
-#python mongo2training.py -o train.json --start-date 2026-01-01
-python mongo2training2.py -o train.json --limit 10000 --verbose
+python mongo2training.py -o train.json --limit 10000 --verbose
