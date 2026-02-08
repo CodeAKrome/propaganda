58a5d65aa153585185d2e98429e640fdbe342f65
kyle gaspar <kyleg.job@gmail.com>
2026-02-08 17:38:09 -0500

    chmod

--- t5/mongo2training.py
+++ t5/mongo2training.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 MongoDB to Bias Training Data Exporter
 =======================================
--- t5/test_mongo2training.sh
+++ t5/test_mongo2training.sh
@@ -1,2 +1,2 @@
 export MONGODB_URI="mongodb://root:example@localhost:27017/"
-python mongo2training.py -o train.json --limit 10000 --verbose
+./mongo2training.py -o train.json --limit 10000 --verbose
